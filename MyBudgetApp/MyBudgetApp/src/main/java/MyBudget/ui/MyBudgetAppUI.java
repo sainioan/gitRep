cc795be68e685376d53de6379c1b2467f408b997
sainioan <sainio.anniina@gmail.com>
2019-11-15 13:02:04 +0200

    changes to mybudgetapp

---
+++ MyBudgetApp/MyBudgetApp/src/main/java/MyBudget/ui/MyBudgetAppUI.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyBudget.ui;
+import java.time.Duration;
+
+import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+
+import java.time.format.DateTimeFormatter;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author ralahtin
+ */
+public class MyBudgetAppUI extends Application {
+    @Override 
+    public void start(Stage stage){
+        
+    }public static void main(String args){
+    launch(args);
+}
+}
--- MyBudgetApp/MyBudgetApp/src/main/java/mybudgetapp/UserDao.java
+++ MyBudgetApp/MyBudgetApp/src/main/java/mybudgetapp/UserDao.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mybudgetapp;
 
+package mybudgetapp;
+import java.util.List;
 /**
  *
  * @author ralahtin
  */
 public interface UserDao {
-    
+    User create(User user) throws Exception;
+    User findByUsername(String username);
+    List<User>getAll();
 }
