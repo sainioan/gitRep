570550e698840fc51c42691877a90d882b0ce03f
Anniina Sainio <anniinasainio@Anniinas-MacBook-Pro.local>
2019-11-17 17:01:06 +0200

    changes in the gui

--- MyBudgetApp/MyBudgetApp/src/main/java/MyBudget/ui/MyBudgetAppUI.java
+++ MyBudgetApp/MyBudgetApp/src/main/java/MyBudget/ui/MyBudgetAppUI.java
@@ -5,6 +5,8 @@
  */
 package MyBudget.ui;
 import mybudgetapp.domain.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -53,10 +55,12 @@ public class MyBudgetAppUI extends Application {
     private VBox myBudgetNodes;
     private Label menuLabel;
     private MyBudgetService mbs;
-    User user;
+//    User user;
     // get username and password from user.
-    String username = user.getUsername();
-    String password = user.getPassword();
+   // String username = user.getUsername();
+    String username = "testUser";
+ //   String password = user.getPassword();
+    String password = "TU123";
     String checkUser, checkPw;
         
 
@@ -64,67 +68,49 @@ public class MyBudgetAppUI extends Application {
     public void start(Stage primarystage) throws Exception {
     primarystage.setTitle("MyBudgetApp");
     
-    BorderPane bp = new BorderPane();
-    bp.setPadding(new Insets(10,50,50,50));
-    
-    HBox inputPane = new HBox();    
-    inputPane.setPadding(new Insets(20,20,20,30));
-    
-    GridPane gridpane = new GridPane();
-    gridpane.setPadding(new Insets(20,20,20,20));
-    gridpane.setHgap(5);
-    gridpane.setVgap(5);
-   
-    Label usernameLabel = new Label("Username"); 
-    TextField usernameInput = new TextField();
-    Label passwordLabel = new Label("Password");
-    PasswordField passwordInput = new PasswordField();
-    Button loginButton = new Button("Login");
-    Label loginMessage = new Label();
- 
- //   Button signupButton = new Button("Sign Up");
-    gridpane.add(usernameLabel, 0,0);
-    gridpane.add(usernameInput, 1,0);
-    gridpane.add(passwordLabel, 0,1);
-    gridpane.add(passwordInput, 1,1);
-    gridpane.add(loginButton,2,1);
-    gridpane.add(loginMessage,1,2);
-    
-    Reflection reflection = new Reflection();
-    reflection.setFraction(0.7f);
-    gridpane.setEffect(reflection);
-    
-    DropShadow dropShadow = new DropShadow();
-        dropShadow.setOffsetX(5);
-        dropShadow.setOffsetY(5);
-           Text text = new Text("MyBudgetApp Login");
-        text.setFont(Font.font("Courier New", FontWeight.BOLD, 28));
-        text.setEffect(dropShadow);
-   inputPane.getChildren().add(text);
-   bp.setId("bp");
-   gridpane.setId("root");
-   loginButton.setId("loginButton");
-   text.setId("text");
-   loginButton.setOnAction((event)-> {
-              checkUser = usernameInput.getText().toString();
-          checkPw = passwordInput.getText().toString();
-          if(checkUser.equals(username) && checkPw.equals(password)){
-           loginMessage.setText("Login successful!");
-           loginMessage.setTextFill(Color.GREEN);
-          }
-          else{
-           loginMessage.setText("Incorrect username or password.");
-           loginMessage.setTextFill(Color.RED);
-          }
-          usernameInput.setText("");
-          passwordInput.setText("");
-         
-         });
-   
-//     loginButton.setOnAction(new EventHandler<ActionEvent>(){
-//         @Override
-//         public void handle(ActionEvent event) {
-//          checkUser = usernameInput.getText().toString();
+  BorderPane bp = new BorderPane();
+//    bp.setPadding(new Insets(10,50,50,50));
+//    
+//    HBox inputPane = new HBox();    
+//    inputPane.setPadding(new Insets(20,20,20,30));
+//    
+//    GridPane gridpane = new GridPane();
+//    gridpane.setPadding(new Insets(20,20,20,20));
+//    gridpane.setHgap(5);
+//    gridpane.setVgap(5);
+//   
+//    Label usernameLabel = new Label("Username"); 
+//    TextField usernameInput = new TextField();
+//    Label passwordLabel = new Label("Password");
+//    PasswordField passwordInput = new PasswordField();
+//    Button loginButton = new Button("Login");
+//    Label loginMessage = new Label();
+// 
+//
+//    gridpane.add(usernameLabel, 0,0);
+//    gridpane.add(usernameInput, 1,0);
+//    gridpane.add(passwordLabel, 0,1);
+//    gridpane.add(passwordInput, 1,1);
+//    gridpane.add(loginButton,2,1);
+//    gridpane.add(loginMessage,1,2);
+//    
+//    Reflection reflection = new Reflection();
+//    reflection.setFraction(0.7f);
+//    gridpane.setEffect(reflection);
+//    
+//    DropShadow dropShadow = new DropShadow();
+//        dropShadow.setOffsetX(5);
+//        dropShadow.setOffsetY(5);
+//           Text text = new Text("MyBudgetApp Login");
+//        text.setFont(Font.font("Courier New", FontWeight.BOLD, 28));
+//        text.setEffect(dropShadow);
+//   inputPane.getChildren().add(text);
+//   bp.setId("bp");
+//   gridpane.setId("root");
+//   loginButton.setId("loginButton");
+//   text.setId("text");
+//   loginButton.setOnAction(e-> {
+//              checkUser = usernameInput.getText().toString();
 //          checkPw = passwordInput.getText().toString();
 //          if(checkUser.equals(username) && checkPw.equals(password)){
 //           loginMessage.setText("Login successful!");
@@ -136,24 +122,26 @@ public class MyBudgetAppUI extends Application {
 //          }
 //          usernameInput.setText("");
 //          passwordInput.setText("");
-//         }
+//         
 //         });
-
-   bp.setTop(inputPane);
-        bp.setCenter(gridpane);  
-         
+//   
+//
+//
+//   bp.setTop(inputPane);
+//        bp.setCenter(gridpane);  
+//         
         //Adding BorderPane to the scene and loading CSS
      Scene scene = new Scene(bp);
-    scene.getStylesheets().add(getClass().getClassLoader().getResource("login.css").toExternalForm());
+    //scene.getStylesheets().add(getClass().getClassLoader().getResource("login.CSS.css").toExternalForm());
      primarystage.setScene(scene);
-       primarystage.titleProperty().bind(
-                 scene.widthProperty().asString().
-                 concat(" : ").
-                 concat(scene.heightProperty().asString()));
-     primarystage.setResizable(false);
+//       primarystage.titleProperty().bind(
+//                 scene.widthProperty().asString().
+//                 concat(" : ").
+//                 concat(scene.heightProperty().asString()));
+//     primarystage.setResizable(false);
      primarystage.show();
     }
-        public static void main(String[] args){
+     public static void main(String[] args){
     launch(args);
 }
 }
