6ee36d45f22924579e7cf38e5b3dfb8e9e50129a
Anniina Sainio <anniinasainio@Anniinas-MacBook-Pro.local>
2019-11-18 20:06:16 +0200

    monopoly class diagram

--- MyBudgetApp/MyBudgetApp/src/main/java/mybudgetapp/domain/Category.java
+++ MyBudgetApp/MyBudgetApp/src/main/java/mybudgetapp/domain/Category.java
@@ -10,5 +10,27 @@ package mybudgetapp.domain;
  * @author anniinasainio
  */
 public class Category {
-    
+
+      private String value;
+  private String label;
+  public Category (String value, String label) { 
+    this.value = value;
+    this.label = label;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
 }
