e94b80b9dc6646b3b6b28260b1ef3123c77832e7
sainioan <sainio.anniina@gmail.com>
2019-12-14 15:16:25 +0200

    tableview

--- MyBudgetApp/MyBudgetApp/src/main/java/mybudgetapp/domain/MyBudgetService.java
+++ MyBudgetApp/MyBudgetApp/src/main/java/mybudgetapp/domain/MyBudgetService.java
@@ -5,6 +5,7 @@
  */
 package mybudgetapp.domain;
 
+import java.sql.Connection;
 import javafx.beans.property.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,8 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -20,10 +23,14 @@ import java.util.Collections;
 import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 import mybudgetapp.dao.BudgetDao;
 import mybudgetapp.dao.DBBudgetDao;
 import mybudgetapp.dao.DBUserDao;
@@ -45,6 +52,8 @@ public class MyBudgetService {
     private String password;
     private LocalDate date;
     private Balance currentBalance;
+    private ObservableList<ObservableList> data;
+    private TableView tableview;
 
     /**
      *
@@ -323,6 +332,56 @@ public class MyBudgetService {
             return true;
         }
 
+    }
+      public void buildData(User user) throws SQLException, Exception {
+        Connection c;
+        data = FXCollections.observableArrayList();
+        try {
+            c = mybDatabase.connect();
+            String SQL = "SELECT*from BALANCE WHERE user_username = ?";
+            PreparedStatement stmt = c.prepareStatement(SQL);
+            //ResultSet
+            stmt.setString(1, user.getUsername());
+            ResultSet rs = stmt.executeQuery();
+
+            /**
+             * ********************************
+             * TABLE COLUMN ADDED DYNAMICALLY * ********************************
+             */
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tableview.getColumns().addAll(col);
+                System.out.println("Column [" + i + "] ");
+            }
+
+            /**
+             * ******************************
+             * Data added to ObservableList * ******************************
+             */
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+            }
+            //FINALLY ADDED TO TableView
+            tableview.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error on Building Data");
+        }
     }
 
 }
